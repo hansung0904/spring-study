cdeb17b124f4f60293d113b489f4428ea001701d
hansung0904 <hansung0904@naver.com>
2022-10-07 00:14:50 +0900

    SpringStudyDay1 at home

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface MemberRepository {
     Member save (Member member);
     Optional<Member> findById(Long id); // null이 반환될 수 있는걸 Optional로 감싸서 반환해주는역할
