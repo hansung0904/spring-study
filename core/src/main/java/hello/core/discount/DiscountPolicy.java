255d9aa34e526bb3b051529c8919bef5c5cb6af2
최한성 <hansung0904@naver.com>
2022-10-11 11:49:03 +0900

    AppConfigCreate & AppConfigRefactor second commit

--- core/src/main/java/hello/core/discount/DiscountPolicy.java
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -7,6 +7,6 @@ public interface DiscountPolicy {
     /**
      * @return 할인 대상 금액
      **/
-    int discount(Member member, int price);
+    int discount(Member member, int price); // member를 파라미터로 넘기기만 함 어떤한 의존관계도 없음
 
 }
