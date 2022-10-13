93cb99737e1aef09c3406b02ed7c80c989f091a5
최한성 <hansung0904@naver.com>
2022-10-13 20:27:32 +0900

    Spring Filter commit

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // ComponentScan으로 다 찾아서 스프링 빈으로 다 등록해주는데 그 중에서 제외할것들을 지정해주는 것
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // ComponentScan으로 다 찾아서 스프링 빈으로 다 등록해주는데 그 중에서 제외할것들을 지정해주는 것
 )
 public class AutoAppConfig {
-
 }
