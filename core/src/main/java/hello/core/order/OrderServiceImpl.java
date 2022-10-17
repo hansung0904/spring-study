8ac18c6008da2f822763b9f5da3231dc82e07f84
최한성 <hansung0904@naver.com>
2022-10-17 09:23:43 +0900

    Create Annotation Practice

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,26 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은걸 가지고 생성자를 만들어주는 annotation
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository; // private final로 해놓은이유 -> memberRepository얘는 무조건 값이 존재하여야만 함.
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    private DiscountPolicy rateDiscountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
