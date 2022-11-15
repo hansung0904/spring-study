77f579b408285376797ef4584e2ce4dbc05858db
최한성 <hansung0904@naver.com>
2022-11-15 17:33:47 +0900

    Day6 : HTTP 요청 파라미터 - @RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,10 +3,13 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,57 @@ public class RequestParamController {
         log.info("username = {}, age = {}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody //RestController랑 같은 효과
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+
+        log.info("username = {}, age = {}", memberName, memberAge);
+        return "OK";
+    }
+
+    @ResponseBody //RestController랑 같은 효과
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody //RestController랑 같은 효과
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody //RestController랑 같은 효과
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username, //username이 꼭 들어와야함 false면은 없어도 된다.
+            @RequestParam(required = false) Integer age) { //null이 들어가기 때문에 Integer타입으로 선언해주어야함.
+
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody //RestController랑 같은 효과
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username, //username이 꼭 들어와야함 false면은 없어도 된다.
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+
+        log.info("username = {}, age = {}", username, age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "OK";
     }
 }
